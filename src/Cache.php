badff566d81fdf8c8842450033997441df4e4918
katheroine <katheroine@gmail.com>
2026-04-22 05:09:30 +0200

    setsDataWithProperKey: false

---
+++ src/Cache.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Katarzyna Krasińska
+ * PHP.PSR-16.lab - https://github.com/katheroine/php.psr-16.lab
+ * Licensed under GPL-3.0 - see LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace PhpLab\StandardPsr16;
+
+class Cache
+{
+}
